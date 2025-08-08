501f2489edf920095fe125562ea619b373672568
akhil <akhilmengi7@gmail.com>
2025-08-08 16:44:40 +0530

    cors bug fix

--- src/app.js
+++ src/app.js
@@ -8,9 +8,13 @@ const PORT = 3000;
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+    origin: [
+        "http://localhost:5173",
+        "http://16.16.209.95"
+    ],
+    credentials: true
+}));
+
 
 app.use("/api",botRouter)
 app.get("/", (req, res) =>{
